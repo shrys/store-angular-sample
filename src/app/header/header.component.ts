7c1aff054e7dd9c9fe3bb2aaeab327ffa18c8b9c
shrys <shreyas770@yahoo.in>
2018-09-11 14:59:34 +0530

    store recipes on firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
@@ -6,7 +7,17 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
     menuToggle = false;
 
+    constructor(private dataStorageService: DataStorageService) {}
+
     toggleMenu() {
         this.menuToggle = !this.menuToggle;
+    }
+
+    onSaveData() {
+        this.dataStorageService.storeRecipes().subscribe(
+            response => {
+                console.log(response.text());
+            }
+        );
     }
 }
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable()
+export class DataStorageService {
+    constructor(private http: Http, private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        return this.http.put('https://ng-recipe-book-9e371.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    }
+}
