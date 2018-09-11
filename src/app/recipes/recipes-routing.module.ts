e1697e53cdec0c639450fac45339795be27dfdac
shrys <shreyas770@yahoo.in>
2018-09-11 19:02:41 +0530

    shifting providers services

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -19,6 +19,7 @@ const recipeRoutes: Routes = [
     imports: [
         RouterModule.forChild(recipeRoutes)
     ],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [AuthGuard]
 })
 export class RecipeRoutingModule {}
