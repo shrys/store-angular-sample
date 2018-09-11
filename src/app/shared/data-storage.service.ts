6e063b963c376410f2bcbf58735cb000b01a0c16
shrys <shreyas770@yahoo.in>
2018-09-11 17:08:47 +0530

    auth with token

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,17 +3,20 @@ import { Http } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { map } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) {}
+    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {}
 
     storeRecipes() {
         return this.http.put('https://ng-recipe-book-9e371.firebaseio.com/recipes.json', this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipe-book-9e371.firebaseio.com/recipes.json')
+        const token = this.authService.getToken();
+
+        this.http.get('https://ng-recipe-book-9e371.firebaseio.com/recipes.json?auth=' + token)
         .pipe(
             map(
                 (response) => {
